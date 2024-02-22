6cf204c792a5c5afbb0887fe0a74b04706d85aaf
PriyumP <126Ppatel@frhsd.com>
2024-02-22 13:37:48 -0500

    Update Student.java

--- Student.java
+++ Student.java
@@ -2,28 +2,59 @@ import java.util.ArrayList;
 public class Student
 {
     private String name;
-    private ArrayList<String> assignments;
+    private ArrayList<String> assignmentNames;
+    private ArrayList<Double> assignmentGrades;
     private double grade;
+
     public Student(String name)
     {
         this.name = name;
-        this.assignments = new ArrayList<String>();
+        this.assignmentNames = new ArrayList<String>();
+        this.assignmentGrades = new ArrayList<Double>();
     }
     public void addAssignment(String assignmentName)
     {
-        String assignment = new String(assignmentName);
-        assignments.add(assignmentName);
+        assignmentNames.add(assignmentName);
+        assignmentGrades.add(null);
     }
     public void  gradeAssignment(String assignment, double grade)
     {
-        for(String a: assignments)
+        int index = assignmentNames.indexOf(assignment);
+        if(index != -1)
         {
-            if(assignments.contains(assignment))
-            {
-                
-            }
+            assignmentGrades.set(index,grade);
         }
     }
+
+    public Double getAssignmentGrade(String assignment)
+    {
+        int index = assignmentNames.indexOf(assignment);
+        if(index != -1)
+        {
+            double assignmentGrade = assignmentGrades.get(index);
+            return assignmentGrade;
+        }
+        return null;
+    }
+    public double getOverallGrade()
+    {
+        double total = 0.0;
+        int numGradedAssignments = 0;
+        for(Double grade: assignmentGrades)
+        {
+            if(grade != null)
+            {
+                total += grade;
+                numGradedAssignments++;
+            }
+        }
+        return total/numGradedAssignments;
+    }
+    public String toString()
+    {
+        return "Name: " + name + "Average: " + getOverallGrade();
+    }
+
 
     public double getGrade()
     {
